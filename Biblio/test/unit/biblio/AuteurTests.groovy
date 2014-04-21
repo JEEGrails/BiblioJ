81280a420bf246a9c4e77d134304ceee31ca2365
elmahdibenhadda <elmahdi.benhadda@gmail.com>
2014-04-21 03:56:03 +0200

    **Pair programing** Ajout de teste unitaire pour classe de domaine

--- Biblio/test/unit/biblio/AuteurTests.groovy
+++ Biblio/test/unit/biblio/AuteurTests.groovy
@@ -11,7 +11,43 @@ import org.junit.*
 @TestFor(Auteur)
 class AuteurTests {
 
-    void testSomething() {
-       fail "Implement me"
+   void testConstraintNomNullPrenomNull() {
+		def auteur = new Auteur()
+		assertFalse auteur.validate()
     }
+
+	void testConstraintPrenomNull() {
+		def auteur = new Auteur(nom : "Nom")
+		assertFalse auteur.validate()
+	}
+
+	void testConstraintNomNull() {
+		def auteur = new Auteur(prenom : "Prenom")
+		assertFalse auteur.validate()
+	}
+
+	void testConstraintNomBlank() {
+		def auteur = new Auteur(nom : "", prenom : "Prenom")
+		assertFalse auteur.validate()
+	}
+
+	void testConstraintPrenomBlank() {
+		def auteur = new Auteur(nom : "Nom", prenom : "")
+		assertFalse auteur.validate()
+	}
+
+	void testConstraintNomBlankPrenomBlanck() {
+		def auteur = new Auteur(nom : "", prenom : "")
+		assertFalse auteur.validate()
+	}
+
+	void testConstraintsValidees() {
+		def auteur = new Auteur(nom : "Nom", prenom : "Prenom")
+		assertTrue auteur.validate()
+	}
+
+	void testToString() {
+		def auteur = new Auteur(nom : "Nom", prenom : "Prenom")
+		assertEquals("Nom Prenom", auteur.toString())
+	}
 }
