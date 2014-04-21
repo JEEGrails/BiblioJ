2f07c95e168bbc9efddfedaa549ddc766df4e116
elmahdibenhadda <elmahdi.benhadda@gmail.com>
2014-04-21 03:56:03 +0200

    **Pair programing** Ajout de teste unitaire pour classe de domaine**

---
+++ Biblio/test/unit/biblio/TypeDocumentTests.groovy
@@ -0,0 +1,36 @@
+package biblio
+
+import static org.junit.Assert.*
+
+import grails.test.mixin.*
+import grails.test.mixin.support.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
+ */
+@TestMixin(GrailsUnitTestMixin)
+class TypeDocumentTests {
+
+ 
+
+    void testConstraintIntituleNull() {
+       def typeDocument = new TypeDocument()
+	   assertFalse typeDocument.validate()
+    }
+
+	void testConstraintIntituleBlank() {
+		def typeDocument = new TypeDocument(intitule : "")
+		assertFalse typeDocument.validate()
+	}
+
+	void testConstraintsValidees() {
+		def typeDocument = new TypeDocument(intitule : "Histoire")
+		assertTrue typeDocument.validate()
+	}
+	
+	void testToString() {
+		def typeDocument = new TypeDocument(intitule : "Histoire")
+		assertEquals("Histoire", typeDocument.toString())
+	}
+}
